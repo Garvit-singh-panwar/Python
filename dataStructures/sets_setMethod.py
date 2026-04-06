89d0d5572044cf9d8f30c06512bef396954c46a6
Garvit Singh Panwar <garvits893@gmail.com>
2026-04-07 01:37:03 +0530

    update sets information

--- dataStructures/sets_setMethod.py
+++ dataStructures/sets_setMethod.py
@@ -22,6 +22,24 @@ print(
 
     pop method 
     pop method removes the random element from the set
+
+
+    Set Operations:-
+    Union , intersection 
+    let say we have a set a  and a set b 
+    if we want to store all the elements of and and b in a set we use unioun method 
+    c = a.union(b) it returns set of unique elements of both set
+
+    intersection method let say we have a set a  and a set b 
+    if we want a set which contain element which is present in both a and b we use intersection 
+    d = a.intersection(b)
+
+    let say a = {1,2,3,4,5,6,7}
+            b = {5,6,7,8,9,10} 
+
+            a.union(b) = {1,2,3,4,5,6,7,8,9,10}
+            a.intersection(b) = {5,6,7} 
     
+    this is same as sets in mathematics 
     '''
 )
